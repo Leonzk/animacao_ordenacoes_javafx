5045b210074ccc1989ee920432e7815fdb9c5732
Leonzk <leobocari@gmail.com>
2025-09-09 21:11:23 -0300

    feat: init repository

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module leonardo_wilker.po.trab_animacao {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens leonardo_wilker.po.trab_animacao to javafx.fxml;
+    exports leonardo_wilker.po.trab_animacao;
+}
